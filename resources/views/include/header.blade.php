3bca8fed549929a0c7af2c3c1cff4cb9a012642b
tiemotijger83 <tiemotijger83@gmail.com>
2025-11-25 19:38:25 +0100

    css upgrade

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -7,19 +7,17 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-  <header>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-    <div class="navbar-header">
-    <img class="navbar-img" src="" alt="">
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="{{ url('/') }}">Home</a></li>
-      <li><a href="{{ url('/secondpage') }}">secondpage</a></li>
-      <li><a href="{{ url('/') }}"></a>nog niks</li>
-      <li><a href="#">niks</a></li>
+<header class="col">
+  <img id="logo" class="col-2" src=" {{ asset('images/logoONC.png') }}" alt="Logo">
+  <nav id="navHome" class="col-10 bg-light">
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <a class="nav-link border border-dark rounded-5 active" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link border border-dark rounded-5" href="/secondpage">Secondpage</a>
+      </li>
     </ul>
-  </div>
-</nav>
+  </nav>
 </header>
 
