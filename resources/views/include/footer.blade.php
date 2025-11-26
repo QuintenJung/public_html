1f9f29c2fdd35b2129abe0166966e96bf2d4124f
Quizmaster08 <9024671@student.tcrmbo.nl>
2025-11-26 01:00:37 +0100

    fotos fixed

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="bg-light flex">
     <div class="col-3 p-3">
-        <div class="bg-primary border rounded-3 p-2 ps-4">
+        <div class="bg-primary border rounded-3 p-2 ps-4"
+            style="background-image: url('{{ asset('images/home.png') }}');">
             <p class="text-light">
             <h4>Adres</h4>
             Oranje Nassau College Parkdreef<br>
@@ -13,14 +14,16 @@
         </div>
     </div>
     <div class="col-3 p-3">
-        <div class="bg-primary border rounded-3 p-2 ps-4">
+        <div class="bg-primary border rounded-3 p-2 ps-4"
+            style="background-image: url('{{ asset('images/info.png') }}');">
             <p class="text-light">
             <h4>Snel naar *info tabs*</h4>
             </p>
         </div>
     </div>
     <div class="col-3 p-3">
-        <div class="bg-primary border rounded-3 p-2 ps-4">
+        <div class="bg-primary border rounded-3 p-2 ps-4"
+            style="background-image: url('{{ asset('images/dev.png') }}');">
             <p class="text-light">
             <h4>devs *links naar dev paginas*</h4>
             Quinten Jungschlager<br>
@@ -30,7 +33,8 @@
         </div>
     </div>
     <div class="col-3 p-3">
-        <div class="bg-primary border rounded-3 p-2 ps-4">
+        <div class="bg-primary border rounded-3 p-2 ps-4"
+            style="background-image: url('{{ asset('images/phone.png') }}');">
             <p class="text-light">
             <h4>Volg ons op *link naar media*</h4>
             </p>
