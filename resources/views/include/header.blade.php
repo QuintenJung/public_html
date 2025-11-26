b7f443f4371bf07068657be30bc5bbbe93d58e05
QuintenJung <9024671@student.tcrmbo.nl>
2025-11-26 14:13:01 +0100

    backup

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -28,6 +28,10 @@ function active($str)
                 <li class="nav-item">
                     <a class="nav-link <?php active('info'); ?>" href="/info">info</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php active('devs'); ?>" href="/devs">*devs</a>
+                </li>
             </ul>
         </nav>
     </header>
+
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/ProfileCardUse.jsx'
+                'resources/js/ProfileCardUse.jsx',
+                'resources/js/CardNavUse.jsx'
             ],
             refresh: true,
         }),
