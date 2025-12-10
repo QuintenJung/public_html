ff29e9e4b0aec8bb7f8832b8c6a870daa8735f79
DeathlyTimo <timosleeuwenhoek@gmail.com>
2025-12-10 14:45:10 +0100

    meer in controlroom

--- resources/views/devs/controlRoom.blade.php
+++ resources/views/devs/controlRoom.blade.php
@@ -6,5 +6,28 @@ $title = 'controlRoom';
 @extends('layouts.def')
 
 @section('content')
+<li>
+    <a href="bug">allBugs</a>
+</li>
+<li>
+    <a href="user">allUser</a>
+</li>
+<li>
+    @if( Auth::user()?->id != "" )
+    <a href="user/{{ Auth::user()?->id }}">accountInfo</a>
+    @endif
+</li>
+<li>
+    <a href="bug/create">createTicket</a>
+</li>
+<li>
+    <a href="/logout">logOut</a>
+</li>
+<li>
+    <a href="user/login">logIn</a>
+</li>
+<P>
+{{ Auth::user()?->name }}
+ </P>
     <p>DEVs</p>
 @endsection
