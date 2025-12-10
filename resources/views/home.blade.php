d73abd080fd57279f23ba644612f660fd2efd70e
tiemotijger83 <tiemotijger83@gmail.com>
2025-12-10 17:06:00 +0100

    help me( video there know)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,17 +23,11 @@ $title = 'home';
         </a>
     </div>
     <div class="content">
-        <video >
-            <source src="{{ asset('images/onc_parkdreef.mp4') }}" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-        <video >
-            <source src="{{ asset('images/onc_parkdreef.mp4') }}" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-        <video>
-            <source src="{{ asset('images/onc_parkdreef.mp4') }}" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
+        <iframe src="https://www.youtube.com/embed/e-ZOmSrgt1w" title="ONC Persoonlijk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+        </iframe>
+        <iframe  src="https://www.youtube.com/embed/YVpC0DwuoQE" title="ONC Excellent" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+        </iframe>
+        <iframe src="https://www.youtube.com/embed/Qyko4rJQrdg" title="ONC Ondernemend" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+        </iframe>
     </div>
 @endsection
