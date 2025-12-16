9b9508eb5bb1e6277e46378bacd584c816de0d6f
DeathlyTimo <timosleeuwenhoek@gmail.com>
2025-12-16 12:55:47 +0100

    more fixes to scaling

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -8,7 +8,7 @@
             Parkdreef 282<br>
             2724 EZ Zoetermeer<br>
             079-341 10 41<br>
-            parkdreef@onc.unicoz.nl<br>
+            orangequest@unicoz.nl<br>
             </p>
         </div>
     </div>
