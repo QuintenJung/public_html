8d7dda1b3f1ed87634f157396baff1956830c41b
tiemotijger83 <tiemotijger83@gmail.com>
2025-12-17 22:33:36 +0100

    made bug button

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,13 +1,12 @@
 <footer>
-    <button class="btn" onclick="my_modal_3.showModal()">open modal</button>
+    <button class="btn btn-circle" onclick="my_modal_3.showModal()"><img src="{{ asset('images/bugReportButton.png') }}" alt="bug"></button>
 <dialog id="my_modal_3" class="modal">
   <div class="modal-box">
     <form method="dialog">
       <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
     </form>
     <h3 class="text-lg font-bold">Hello!</h3>
-    <p class="py-4">Press ESC key or click on ✕ button to close</p>
-  </div>
+    <a href="bug/create">go to bug report</a>
 </dialog>
     <div>
         <div style="background-image: url('{{ asset('images/home.png') }}');">
