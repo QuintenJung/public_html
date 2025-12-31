662a5de52239102e7f9999d995fdbe98c45e2b12
DeathlyTimo <timosleeuwenhoek@gmail.com>
2025-12-31 14:37:46 +0100

    form tel fix

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -34,11 +34,9 @@ function active($str)
                 </li>
                 <div class="dropdown dropdown-end">
                     <div tabindex="0" role="button" class="btn m-1">
-                        <button class="btn btn-square btn-ghost">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                            </svg>
-                        </button>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
                     </div>
                     <ul tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
                         <li><a>Item 1</a></li>
